2e6295986bc33c79a27b7b6cd45ba07313330844
aeeluo <paul.alligier@etu.univ-amu.fr>
2022-05-29 19:22:24 +0200

    PUSH Ajout partie contact Ajout d'une flèche de scroll Modification et ajout de décoration aux titres Responsive Modification du footer Ajout de Jquery Push front fini

--- public/js/index.js
+++ public/js/index.js
@@ -1,62 +1,3 @@
-// jQuery.noConflict();
-// jQuery(document).ready(function () {
-//
-//     if (jQuery(".container").width() < 991.8) {
-//         var divchange1 = (jQuery('.div-change1')).detach();
-//         (jQuery('.divchange2')).append(divchange1);
-//     } else {
-//         var divchange2 = (jQuery('.div-change2')).detach();
-//         (jQuery('.divchange1')).append(divchange2);
-//     }
-//
-// });
-
-/*
-window.onresize = function (){
-    if(window.innerWidth <= 991.8){
-        div1 =
-    }
-}
-*/
-
-/*
-const scrollElements = document.querySelectorAll(".js-scroll");
-
-const elementInView = (el, percentageScroll = 100) => {
-    const elementTop = el.getBoundingClientRect().top;
-
-    return (
-        elementTop <= (window.innerHeight || document.documentElement.clientHeight) * (percentageScroll / 100)
-    );
-};
-
-const displayScrollElement = (element) => {
-    element.classList.add("scrolled")
-}
-
-/!*
-const hideScrollElement = (element) => {
-    element.classList.remove("scrolled");
-};
-*!/
-
-const handleScrollAnimation = () => {
-    scrollElements.forEach((el) => {
-        if(elementInView(el, 100)){
-            displayScrollElement(el);
-        }
-        else{
-            hideScrollElement(el);
-        }
-    })
-}
-
-window.addEventListener('scroll', () => {
-    handleScrollAnimation();
-})
-*/
-
-
 $(document).ready(function () {
 
     const checkScreenWidth = function () {
@@ -155,8 +96,8 @@ $(document).ready(function () {
         if (SERVICE_CHASSIS.is(":visible")) {
             toggleMoreInfoClimText();
             toggleMoreInfoChassisText();
-            SERVICE_CHASSIS.stop().slideToggle("slow");
-            SERVICE_CLIM.stop().slideToggle("slow");
+            SERVICE_CHASSIS.stop().slideToggle();
+            SERVICE_CLIM.stop().slideToggle();
         } else {
             toggleMoreInfoClimText();
             SERVICE_CLIM.stop().slideToggle("slow");
@@ -167,13 +108,16 @@ $(document).ready(function () {
         if (SERVICE_CLIM.is(":visible")) {
             toggleMoreInfoClimText();
             toggleMoreInfoChassisText()
-            SERVICE_CLIM.stop().slideToggle("slow");
-            SERVICE_CHASSIS.stop().slideToggle("slow");
+            SERVICE_CLIM.stop().slideToggle();
+            SERVICE_CHASSIS.stop().slideToggle();
         } else {
             toggleMoreInfoChassisText();
             SERVICE_CHASSIS.stop().slideToggle("slow");
         }
     });
 
-
+    $(window).scroll(function(){
+        $(".arrow").css("opacity", 1 - $(window).scrollTop() / 500);
+        //250 is fade pixels
+    });
 });
